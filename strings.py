22047ed7edea9a38b05dd3e4ec28962b93faac21
Christian <cristian_raul_2@hotmail.com>
2019-02-11 13:26:17 -0500

    Tipos de String en Python

---
+++ strings.py
@@ -0,0 +1,16 @@
+myString = "napay pacha"
+
+#print(dir(myString))
+#myString.title()
+print(myString.title())
+#myString.replace("hola mundo")
+print(myString.replace('hola mundo', 'bye'))
+#contar 
+print(myString.count("a"))
+#Con que empieza la oración
+print(myString.startswith('napay'))
+#Seprar el texto
+print(myString.split())
+print(myString.split("a"))
+#contar caracteres
+print(myString.index("a"))
