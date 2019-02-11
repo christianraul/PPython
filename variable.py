c4a4294feff00dfdc1f259eec0e33e1deb59f112
Christian <cristian_raul_2@hotmail.com>
2019-02-11 13:27:28 -0500

    Definición de variables

---
+++ variable.py
@@ -0,0 +1,19 @@
+#cualquier tipo de datos
+x = 10
+# case sensitive
+name = "Pacha"
+Name = "Pacha"
+
+print(name)
+print(10)
+
+x, book = 100, "Libro"
+print(x, book)
+# CONVENCIONES
+book_name = "Viaje en el tiempo" # Snake case
+bookname = "Viaje en el tiempo" # Camel Case
+BookName = "Viaje en el tiempo" # Pascal Case
+print(book_name)
+
+# CONVECIONES RESTRINGIDAS (En mayúsculas)
+PI = 3.1416
