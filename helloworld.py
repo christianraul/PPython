b09906c2748288b8aa5256c607a84c1c00d6b6cc
Christian <cristian_raul_2@hotmail.com>
2019-02-11 13:19:17 -0500

    Mi primer programa en python

---
+++ helloworld.py
@@ -0,0 +1,24 @@
+print ("Napay Pacha")
+
+#TIPOS DE DATOS
+#datos Int
+print (30)
+#datos String
+print("Napay Pacha")
+#float
+print(30.5)
+# Boolean 
+true
+false
+#lista
+[10, 20, 20]
+["Napay", "Pacha"]
+[10, "Napay", "Pacha"]
+
+#tuples
+(10, 20, 40)
+
+#Dicciionarios
+{"su nombre":"Ra","apellidos":"Fact"}
+
+print(type({"su nombre":"Ra","apellidos":"Fact"}))
