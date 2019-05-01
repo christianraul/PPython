a6a66d096ce88e0c222b375485d40906b43d7da1
christianraul <cristian_raul_2@hotmail.com>
2019-05-01 18:25:14 -0500

    Formula Triángulo

--- formulas/frmtriangulo.py
+++ formulas/frmtriangulo.py
@@ -8,22 +8,24 @@ def calcular(): #Método para hallar el triángulo
 
 raiz = Tk() #define la ventana principal
 raiz.title('Triangulo')
-raiz.geometry('300x300') #anchura y altura
-raiz.configure(bg='beige')
+raiz.geometry('300x200') #anchura y altura
+raiz.configure(bg='gold')
 
 areatri= StringVar()
 
-lblnombre = Label(raiz, text="Ingrese el lado:")
-lblnombre.pack()
+colorfondo="gold"
+colorboton="yellow"
+colorletra="red"
 
-txtlado=Entry(raiz)
-txtlado.pack()
-
-btnmostrar =Button(raiz, text="Calcular", command=calcular)
-btnmostrar.pack()
-
-lblresultado= Label(raiz, textvariable=areatri)
-lblresultado.pack()
+lblti=Label(raiz, text="Área de un Triángulo", bg=colorfondo, fg=colorletra, font=("Ebrima", 22)).place(x=20, y=10)
+lblnombre = Label(raiz, text="Ingrese el lado:", bg=colorfondo, fg=colorletra, font=("Helvetica", 16)).place(x=10, y=60)
+txtlado=Entry(raiz, font=("Arial", 12))
+txtlado.place(x=160, y=65)
+btnmostrar =Button(raiz, text="Calcular", bg=colorboton, command=calcular, font=("Ebrima", 14), anchor="center", activebackground="red").place(x=70, y=100)
+btncerrar =Button(raiz, text="Cerrar", bg="red", command=raiz.quit, font=("Ebrima", 14), anchor="center", activebackground="yellow").place(x=170, y=100)
+lbltitulo=Label(raiz, text="El resultado es: ", bg=colorfondo, font=("Ebrima", 20)).place(x=10, y=150)
+lblresultado= Label(raiz, textvariable=areatri, bg=colorfondo, font=("Ebrima", 20))
+lblresultado.place(x=200, y=150)
 
 raiz.mainloop()
 
